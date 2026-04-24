b3d42fe70be5b4e15d75e01fcba3fd46980cb182
rachittt06 <racguptta06@gmail.com>
2026-04-24 23:01:20 +0530

    Add express properly

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
 
-export default (req, res) => app(req, res);
+// 👇 IMPORTANT (this is what was missing earlier)
+export default (req, res) => {
+  return app(req, res);
+};
