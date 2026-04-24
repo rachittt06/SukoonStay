62480f548ee8c75a73097ec21e235c223be5171b
rachittt06 <racguptta06@gmail.com>
2026-04-24 22:36:39 +0530

    Minimal API test

--- server/server.js
+++ server/server.js
@@ -6,12 +6,4 @@ app.get("/", (req, res) => {
   res.send("API is working fine 🚀");
 });
 
-// 👇 Only run locally
-if (process.env.NODE_ENV !== "production") {
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
-  });
-}
-
-// 👇 For Vercel
 export default (req, res) => app(req, res);
