2c25cb451ec4edee2862235ce20d087abcf03182
rachittt06 <racguptta06@gmail.com>
2026-04-24 22:16:54 +0530

    Fix Vercel API issue

--- server/server.js
+++ server/server.js
@@ -1,23 +1,19 @@
-import express from "express"
+import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import { clerkMiddleware } from "@clerk/express";
 
-connectDB()
+const app = express();
 
-const app = express()
-app.use(cors())
+// connectDB(); // test off first
 
-//Middleware
-app.use(express.json())
-app.use(clerkMiddleware)
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
-//API to listen to clerk webhooks
-app.use("/api/clerk", clerkWebhooks)
+app.get("/", (req, res) => {
+  res.send("API is working fine 🚀");
+});
 
-app.get('/',(req, res)=> res.send("API is working"))
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
+export default app;
